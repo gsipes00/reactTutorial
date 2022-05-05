27d037a2500cb1a74c70a1e9e83a22475b7f6285
Gene Sipes <gsipes00@yahoo.com>
2022-05-04 20:31:41 -0500

    first edit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//component name 'Greeting' must have first letter capitilized
+function Greeting() {
+  return <h1>im Gene and this is my first react app cool!</h1>;
+}
+//'Greeting' tag MUST have a closing tag />
+ReactDom.render(<Greeting />, document.getElementById("root"));
