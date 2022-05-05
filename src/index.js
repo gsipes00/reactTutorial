0e1e70b7b086e8e24c64fae5caf8e2bfea7288e5
Gene Sipes <gsipes00@yahoo.com>
2022-05-05 16:35:08 -0500

    created intro index.html and index.css files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+// css
+import "./index.css";
+
+function Booklist() {
+  return (
+    <section className='booklist'>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
+
+const Book = () => {
+  return (
+    <article className='book'>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+const Image = () => (
+  <img src='https://imgs.search.brave.com/IIQW_a0eTwHXxFEBUInufV40aGMiwOxmPoY2hRwW43s/rs:fit:568:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5r/VWpuaWM1WDE4Q1Vh/MXM2X0JIVHZnSGFH/TCZwaWQ9QXBp' />
+);
+const Title = () => <h1>curl up with a good book.</h1>;
+
+const Author = () => <h4>-Johnny B Goode</h4>;
+
+ReactDom.render(<Booklist />, document.getElementById("root"));
+
 // component name 'Greeting' must have first letter capitilized
 // stateless functional component aka dumb component, does not have state
 // always return JSX
-function Greeting() {
-  return <h1>im Gene and this is my first react app cool!</h1>;
-}
+
+// notes on JSX rules
+// can only return a SINGLE element
+// div / section / article or fragment - samantic html is encouraged
+// use camelCase for html attribute
+// className instead of class for element class
+// close every element
+// formatting
 
 // alternate way to create component with explicit function call more difficult to return html
 // const Greeting = () => {
@@ -16,6 +56,3 @@ function Greeting() {
 //     "im Gene and this is my first React app cool!"
 //   );
 // };
-
-//'Greeting' tag MUST have a closing tag />
-ReactDom.render(<Greeting />, document.getElementById("root"));
