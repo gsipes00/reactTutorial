74fa11f02531ce4963059b975c1e3959697def60
Gene Sipes <gsipes00@yahoo.com>
2022-05-04 20:50:08 -0500

    intro and set up done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-//component name 'Greeting' must have first letter capitilized
+// component name 'Greeting' must have first letter capitilized
+// stateless functional component aka dumb component, does not have state
+// always return JSX
 function Greeting() {
   return <h1>im Gene and this is my first react app cool!</h1>;
 }
+
+// alternate way to create component with explicit function call more difficult to return html
+// const Greeting = () => {
+//   return React.createElement(
+//     "h1",
+//     {},
+//     "im Gene and this is my first React app cool!"
+//   );
+// };
+
 //'Greeting' tag MUST have a closing tag />
 ReactDom.render(<Greeting />, document.getElementById("root"));
